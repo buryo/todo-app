538b5dc8f7be5c114c47074c7707f9ff1145246b
Atilla <k.rebien@gmail.com>
2019-07-20 21:57:17 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Sidebar from './Sidebar.jsx';
 import Example from './Test';
+import Newtask from './newTask/Newtask';
 
 
 class App extends React.Component {
@@ -14,6 +15,7 @@ class App extends React.Component {
           <Sidebar />
           
           <Route path="/" exact />
+          <Route path="/new-task" component={Newtask}/>
           <Route path="/example" component={Example}/>
         </div>
       </Router>
---
+++ src/components/newTask/Newtask.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import './newtask.css';
+
+import Form from "./Form";
+
+ const Newtask = () => {
+  const [todos, setTodos] = useState([]);
+
+  const toggleComplete = i =>
+    setTodos(
+      todos.map(
+        (todo, k) =>
+          k === i
+            ? {
+                ...todo,
+                complete: !todo.complete
+              }
+            : todo
+      )
+    );
+
+  return (
+    <div className="App">
+      <Form
+        onSubmit={text => setTodos([{ text, complete: false }, ...todos])}
+      />
+      <div>
+        {todos.map(({ text, complete }, i) => (
+          <div
+            key={text}
+            onClick={() => toggleComplete(i)}
+            style={{
+              textDecoration: complete ? "line-through" : ""
+            }}
+          >
+            {text}
+          </div>
+        ))}
+      </div>
+      {/* The button to setState to null */}
+      {/* <button onClick={() => setTodos([])}>reset</button> */}
+    </div>
+  );
+};
+
+export default Newtask;
