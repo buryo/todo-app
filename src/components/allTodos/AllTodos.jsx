bd5352506b9e955645abf5deba9bd28570bdf2a6
Atilla <k.rebien@gmail.com>
2019-07-21 17:22:04 +0200

    Delete option now works, a user can delete one todo

--- src/components/allTodos/AllTodos.jsx
+++ src/components/allTodos/AllTodos.jsx
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 import { Icon, Button } from "antd";
 import "./alltodos.css";
+import ColumnGroup from "antd/lib/table/ColumnGroup";
 
 const AllTodos = () => {
     const allLocalTodos = JSON.parse(localStorage.getItem("Todos")) || '';
     const [todos, setTodos] = useState([...allLocalTodos]);
-
-    const handleDelete = () => {
-        todos(null)
-    }
     
+
+    console.log(allLocalTodos);
+
+    // the function to filter trough the todos state and remove unmatching id and proceed to set the localStorage to new adjusted object
+      
+    const handleDelete = id => {
+      const matchTodo = todos.filter(todo => todo.id !== id) 
+      setTodos(matchTodo)
+
+      localStorage.setItem('Todos', JSON.stringify(matchTodo));
+
+    };
+    
+
     return (
         <main>
-            {todos.map(({ title, complete, date }, i) => (
+            {todos.map(({ id, title, complete, date }, i) => (
                 <div
-                    key={title}
+                    key={id}
                     // onClick={() => toggleComplete(i)}
                     style={{
                         textDecoration: complete ? "line-through" : ""
@@ -23,21 +34,22 @@ const AllTodos = () => {
                     {`${title} - Date: ${date}`}
 
                     {/* the check and delete icon with link */}
-                    <a href="/">
+
                         <Icon
+                            // onClick={() => showId(id)}
                             style={{ paddingLeft: "3em" }}
                             type="check-circle"
                             theme="twoTone"
                             twoToneColor="#52c41a"
                         />
-                    </a>
+
 
                     <Icon
                         style={{ marginRight: "3em" }}
                         type="delete"
                         theme="twoTone"
                         twoToneColor="#ff0000"
-                        onClick={handleDelete}
+                        onClick={() => handleDelete(id)}
                     />
                 </div>
             ))}
