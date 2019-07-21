9439611d8d39cf4f1ca10157c01f0c55335f4470
Atilla <k.rebien@gmail.com>
2019-07-21 13:21:26 +0200

    .

--- src/components/allTodos/AllTodos.jsx
+++ src/components/allTodos/AllTodos.jsx
@@ -5,10 +5,12 @@ import "./alltodos.css";
 const AllTodos = () => {
   const allLocalTodos = JSON.parse(localStorage.getItem("Todos"));
   const [todos, setTodos] = useState([...allLocalTodos]);
+//   console.log(object);
 
-const handleDelete = () => {
-    todos(null)
-}
+//   const handleDelete = id => {
+//     setUsers(users.filter(user => user.id !== id))
+//   }
+
   return (
     <div>
       {todos.map(({ title, complete, date }, i) => (
@@ -22,21 +24,18 @@ const handleDelete = () => {
           {`${title} Date:${date}`}
 
           {/* the check and delete icon with link */}
-          <a href="/">
           <Icon
             // style={{ paddingLeft: "3em" }}
             type="check-circle"
             theme="twoTone"
             twoToneColor="#52c41a"
           />
-          </a>
 
           <Icon
             // style={{ marginRight: "3em" }}
             type="delete"
             theme="twoTone"
             twoToneColor="#ff0000"
-            onClick={handleDelete}
           />
         </div>
       ))}
