b1be3bb1833b54c281bb66ac18ecf51332f9c94d
Atilla <k.rebien@gmail.com>
2019-07-21 09:59:47 +0200

    check icon and delete icon added with link

--- src/components/allTodos/AllTodos.jsx
+++ src/components/allTodos/AllTodos.jsx
@@ -1,23 +1,47 @@
 import React, { useState } from "react";
+import { Icon, Button } from "antd";
+import "./alltodos.css";
 
 const AllTodos = () => {
-    const todos = JSON.parse(localStorage.getItem('Todos'));
-    
-    return (
-        <div>
-            {todos.map(({ title, complete, date }, i) => (
-                <div
-                    key={title}
-                    // onClick={() => toggleComplete(i)}
-                    style={{
-                        textDecoration: complete ? "line-through" : ""
-                    }}
-                >
-                    {`${title} Date:${date}`}
-                </div>
-            ))}
-        </div>
-    );
+  const allLocalTodos = JSON.parse(localStorage.getItem("Todos"));
+  const [todos, setTodos] = useState([...allLocalTodos]);
+
+const handleDelete = () => {
+    todos(null)
 }
+  return (
+    <div>
+      {todos.map(({ title, complete, date }, i) => (
+        <div
+          key={title}
+          // onClick={() => toggleComplete(i)}
+          style={{
+            textDecoration: complete ? "line-through" : ""
+          }}
+        >
+          {`${title} Date:${date}`}
+
+          {/* the check and delete icon with link */}
+          <a href="/">
+          <Icon
+            // style={{ paddingLeft: "3em" }}
+            type="check-circle"
+            theme="twoTone"
+            twoToneColor="#52c41a"
+          />
+          </a>
+
+          <Icon
+            // style={{ marginRight: "3em" }}
+            type="delete"
+            theme="twoTone"
+            twoToneColor="#ff0000"
+            onClick={handleDelete}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default AllTodos;
