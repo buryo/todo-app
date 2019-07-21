32c599b3ceb6754894582b4e763e3cdd9a5dbd58
Atilla <k.rebien@gmail.com>
2019-07-22 00:42:16 +0200

    the video background that only works with an URL

--- src/components/allTodos/AllTodos.jsx
+++ src/components/allTodos/AllTodos.jsx
@@ -6,46 +6,6 @@ import "./alltodos.css";
 const AllTodos = () => {
     const allLocalTodos = JSON.parse(localStorage.getItem("Todos")) || '';
     const [todos, setTodos] = useState([...allLocalTodos]);
-<<<<<<< HEAD
-    
-
-    console.log(allLocalTodos);
-
-    // the function to filter trough the todos state and remove unmatching id and proceed to set the localStorage to new adjusted object
-      
-    const handleDelete = id => {
-      const newTodos = todos.filter(todo => todo.id !== id) 
-      setTodos(newTodos)
-
-      localStorage.setItem('Todos', JSON.stringify(newTodos));
-
-    };
-    
-
-    return (
-        <main>
-            {todos.map(({ id, title, complete, date }, i) => (
-                <div
-                    key={id}
-                    // onClick={() => toggleComplete(i)}
-                    style={{
-                        textDecoration: complete ? "line-through" : ""
-                    }}
-                >
-                    {`${title} - Date: ${date}`}
-
-                    {/* the check and delete icon with link */}
-
-                        <Icon
-                            // onClick={() => showId(id)}
-                            style={{ paddingLeft: "3em" }}
-                            type="check-circle"
-                            theme="twoTone"
-                            twoToneColor="#52c41a"
-                        />
-
-=======
->>>>>>> 9883afd88c61fc08fa7d00e2af6767770f6a3bca
 
     const deleteHandler = (e, id) => {
         e.currentTarget.parentNode.parentNode.classList.add('deleting');
